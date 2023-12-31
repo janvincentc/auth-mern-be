71c8cd1eabdfd15fbaaf5428204377e950dda1aa
janvincentc <janvincent.claudio@gmail.com>
2023-12-31 09:34:57 +0800

    added signin function

--- src/api/controllers/auth.controller.js
+++ src/api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js"
+import { errorHandler } from "../utils/error.js"
 import bcryptjs from "bcryptjs"
 
 export const signup = async (req, res, next) => {
@@ -8,6 +9,19 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save()
     res.status(201).json({ message: "Successfully signed up" })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body
+  const validUser = await User.findOne({ email: email })
+  if (!validUser) return next(errorHandler(400, "User not found"))
+  const validPassword = bcryptjs.compare(password, validUser.password)
+  if (!validPassword) return next(errorHandler(401, "Invalid Password"))
+  try {
+
   } catch (error) {
     next(error)
   }
